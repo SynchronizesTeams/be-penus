6a3fbb0a309234f711433509c326c4d0b9e85359
adeleeeeyyyy <mitsuhaadly@gmail.com>
2025-01-20 14:19:18 +0700

    dsnadjkskjadsa

--- routes/api.php
+++ routes/api.php
@@ -44,6 +44,7 @@ Route::middleware('auth:sanctum')->group(function()  {
 
 Route::prefix('v1/misc')->group(function() {
     Route::get('berita', [MainController::class, 'showBeritaAll']);
+    Route::get('berita/{berita_id}', [MainController::class, 'showBerita']);
     Route::get('galeri', [MainController::class, 'showGaleri']);
     Route::get('index', [MainController::class, 'index']);
     Route::get('tag', [MainController::class, 'getTags']);
